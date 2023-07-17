f72c357b26f85c7c75a70db1e99929ec41f8be85
José Luis Landabaso Díaz <landabaso@gmail.com>
2023-07-17 06:59:54 +0200

    Upgrade to playground v1.0.5

--- src/guides/ledgerProgramming.js
+++ src/guides/ledgerProgramming.js
@@ -10,7 +10,7 @@ const LedgerProgramming = () => {
         //Note it is important to add a new tag to the Github project if this stops working:
         //https://github.com/codesandbox/codesandbox-client/issues/6825#issuecomment-1636136804
         <iframe
-          src="https://codesandbox.io/embed/github/bitcoinerlab/playground/tree/v1.0.4/descriptors/ledger?fontsize=13&hidenavigation=1&theme=light&view=split&codemirror=0&expanddevtools=1&editorsize=65&forcerefresh=1"
+          src="https://codesandbox.io/embed/github/bitcoinerlab/playground/tree/v1.0.5/descriptors/ledger?fontsize=13&hidenavigation=1&theme=light&view=split&codemirror=0&expanddevtools=1&editorsize=65&forcerefresh=1"
           title="Descriptors"
           allow="accelerometer; ambient-light-sensor; camera; encrypted-media; geolocation; gyroscope; hid; microphone; midi; payment; usb; vr; xr-spatial-tracking"
           sandbox="allow-forms allow-modals allow-popups allow-presentation allow-same-origin allow-scripts"
